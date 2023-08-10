23532b7c8957161035f1cac6ab484641e15e1bc2
Fernando Melgarejo <waefeydelta@gmail.com>
2023-08-10 02:36:44 -0400

    start repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-firstapp';
+  title:string = 'angular-firstapp';
+  salute:string = 'Hi from Angular';
 }
