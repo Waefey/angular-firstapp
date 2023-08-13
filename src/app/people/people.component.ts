e401eb34ae729d9c0698607aba1ff99840e35cb9
Fernando Melgarejo <fmelgarejo@waefey.cl>
2023-08-13 17:01:42 -0400

    Apply CSS basic

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: 'app-people',
-	templateUrl: './people.component.html'
+	templateUrl: './people.component.html',
+	styleUrls: ['./people.component.css']
 })
 export class PeopleComponent {
 
